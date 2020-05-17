3c7df046c6f8e1efc6ad2a94f28ff6c868f90ae6
Ghore <ghoreua@gmail.com>
2020-05-17 11:18:23 +0300

    Updates

---
+++ precache-manifest.3880720129fdab815516805646ab13b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91088f680b5c30f5fb2ce5dbf3d90c6e",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "49160b596fb957c6eb40",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.2fadd680.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.2fadd680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea012fa900b0b93c6d16",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.0da2fd1d.chunk.js"
+  },
+  {
+    "revision": "cc2f68d2ad3dc0fbeefc",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.ad1484ea.js"
+  }
+]);
